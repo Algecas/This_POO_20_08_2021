2c024c74107ddba1988c715660272618c6557697
Albert Stephen Gerena Cstellanos <algecass94@gmail.com>
2021-09-01 21:33:51 -0500

    Ajuste impresión de datos

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+//Clase Humanos
 class humanos{
     
     constructor(arg){
@@ -11,30 +11,33 @@ class humanos{
     }
 }
 
-
+//Set de datos 1
 let dataClase = {
     cabello: "Morado",
-    altura: 100, 
+    altura: 170, 
     ojos: 23,
     manos: 25,
     dedos: 58,
     pies: 2
 };
 
+//Set de datos 2
 let dataClase2 = {
     cabello: "Morado",
-    altura: 100, 
+    altura: 52, 
     ojos: 23,
     manos: 25,
     dedos: 58,
     pies: 4
 };
 
-//Instanciar clase humanos
+//Instanciar clase humanosset de datos 1
+console.log(`Impresión por clases`);
 const inst = new humanos(dataClase);
-console.log(inst.caminar());
+console.log(inst.caminar(),`Soy humano y mi altura es: ${dataClase.altura} cm`);
+//Instanciar clase humanosset de datos 2
 const inst2 = new humanos(dataClase2);
-console.log(inst2.caminar());
+console.log(inst2.caminar(),`Soy animal y mi altura es: ${dataClase2.altura} cm`);
 
 
 console.warn("Clase", inst2);
@@ -60,11 +63,15 @@ let dataObjeto = {
     dedos: undefined,
     pies: undefined
 }
+
+//Llamado constructor por objetos con inicialización de datos
+console.log(`Impresión por objetos`);
 const humanosObjetos = Object.setPrototypeOf(dataObjeto,humanosObjetosMetodos);
 humanosObjetos.constructorHumanos({pies: 2});
-console.log(humanosObjetos.caminar());
+console.log(humanosObjetos.caminar(),`Soy humano y mi altura es: ${dataObjeto.altura=170} cm`);
+//Llamado constructor por objetos con inicialización de datos 2
 humanosObjetos.constructorHumanos({pies: 4});
-console.log(humanosObjetos.caminar());
+console.log(humanosObjetos.caminar(),`Soy animal y mi altura es: ${dataObjeto.altura=47} cm`);
 
 
 console.warn("Objetos", humanosObjetos);
